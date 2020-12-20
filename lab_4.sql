4640c89ca78f975ad46d0373ca6c07ccbe2c669a
Denys <67656779+Yung-Pyrokinesis@users.noreply.github.com>
2020-12-20 05:30:54 +0200

    Add files via upload

---
+++ lab_4.sql
@@ -0,0 +1,50 @@
+INSERT INTO warehouses (id) VALUES
+  (1);
+
+INSERT INTO employees (id, name, surname, tax_number, position) VALUES 
+  (131, 'Liam', 'Rodriguez', 43212, 'director'),
+  (253, 'Noah', 'Martinez', 43214, 'manager'),
+  (342, 'Oliver', 'Hernandez', 63213, 'loader'),
+  (473, 'William', 'Lopez', 83210, 'loader'),
+  (564, 'Elijah', 'Gonzales', 43216, 'loader'),
+  (644, 'James', 'Wilson', 13217, 'loader'),
+  (777, 'Benjamin', 'Smith', 43218, 'loader'),
+  (853, 'Lucas', 'Johnson', 93210, 'loader'),
+  (953, 'Mason', 'Williams', 43210, 'chauffeur'),
+  (120, 'Ethan', 'Brown', 43219, 'chauffeur');
+
+INSERT INTO date_of_transfer (date) VALUES 
+  ('2020-06-12 10:34:09'),
+  ('2020-07-13 12:24:19'),
+  ('2020-07-22 22:59:36'),
+  ('2020-09-04 06:34:00'),
+  ('2020-09-27 23:51:53'),
+  ('2020-10-29 17:05:44');
+  
+INSERT INTO date_of_receipt (date) VALUES 
+  ('2020-05-16 17:04:34'),
+  ('2020-07-05 12:24:19'),
+  ('2020-07-12 07:36:08'),
+  ('2020-08-30 20:54:45'),
+  ('2020-09-25 18:30:43'),
+  ('2020-10-27 17:25:13');
+
+INSERT INTO setteleme (id, price) VALUES 
+  (245420, '23000'),
+  (354867, '44565'),
+  (407898, '78564');
+  (500076, '9455'),
+  (645664, '10222'),
+  (743684, '15640'),
+
+
+INSERT INTO provider (id, name) VALUES 
+  (1545, 'Metal'),
+  (2564, 'Stal'),
+  (3457, 'Syrovyna Karpaty');
+  
+INSERT INTO contract (id, name, raw_or_materials) VALUES 
+  (34244745, 'Metal'),
+  (46424754, 'Stal'),
+  (35475442, 'Syrovyna');
+  
